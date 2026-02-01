60bfdf9e8749b7f5ec2cfe46f51b6918decfabec
Jim495217 <jimmel.chambers.jr@uhsp.edu>
2026-02-01 14:03:41 -0600

    Update ProductDetailsPage.jsx

--- buzzboard/src/pages/ProductDetailsPage.jsx
+++ buzzboard/src/pages/ProductDetailsPage.jsx
@@ -2,9 +2,10 @@ const ProductDetailsPage = () => {
   return (
     <div style={{ padding: "40px" }}>
       <h2>Product Details</h2>
-      <p>Product details will be displayed here.</p>
+      <p>Product details will be right here!.</p>
     </div>
   );
 };
 
 export default ProductDetailsPage;
+
