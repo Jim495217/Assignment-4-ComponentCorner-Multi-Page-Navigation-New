0c0075840386107eff37346e9a06055126214446
Jim495217 <jimmel.chambers.jr@uhsp.edu>
2026-02-01 14:02:30 -0600

    Update ProductDetailsPage.jsx

---
+++ buzzboard/src/pages/ProductDetailsPage.jsx
@@ -0,0 +1,10 @@
+const ProductDetailsPage = () => {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>Product Details</h2>
+      <p>Product details will be displayed here.</p>
+    </div>
+  );
+};
+
+export default ProductDetailsPage;
